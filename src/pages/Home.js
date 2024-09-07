f125fae2c0673708a1347472705dfdddb971c153
apple <mniksaite@gmail.com>
2024-09-07 13:35:22 +0300

    removed class

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () =>{
 
     return(
         <div className="d-flex  justify-content-end grow-3 mt-4 font-size-20 ">
-            <div className="w-75 d-flex  flex-column gap-3 height overflow ">
+            <div className="w-75 d-flex  flex-column gap-3 ">
                 <div>
                     {t('first')}
                 </div>
